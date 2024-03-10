3621aebbc783ba1d841ffc363b5778d29a86e1a7
Choi <wldnd12223@gmail.com>
2024-03-10 12:12:16 +0900

    update
    update

---
+++ intro/js/footer.js
@@ -0,0 +1,47 @@
+const footer = document.getElementById('footer_template');
+footer.innerHTML = `
+<footer class="footer-99382" >
+			<div class="container">
+			<div class="row">
+			<div class="col-md-7 pr-md-7">
+			<a href="#" class="footer-site-logo d-block mb-4">글로리 법무법인</a>
+			<p style="line-height:180%;">상호 : 법무법인 글로리<br>사업자등록번호 : 604-86-02992<br>대표변호사 : 이아무<br>주소 : 대전광역시 서구 둔산중로 78번길 26, 민석타워 3층 301호<br>대표번호 : 1544-0904(대표번호) / 042-721-0608(형사센터) / 042-721-0606(송무)<br>팩스 : 042-721-0707<br>이메일 : glory@glorylawfirm.kr</p>
+			</div>
+			<div class="col-md">
+			<h3>카테고리</h3>
+			<ul class="list-unstyled nav-links">
+			<li><a href="./case1.html">경찰 & 검찰 수사단계</a></li>
+			<li><a href="./case2.html">법원단계</a></li>
+			<li><a href="./location.html">오시는길</a></li>
+			<li><a href="./consulting.html">긴급상담</a></li>
+			</ul>
+			</div>
+			<div class="col-md">
+			<h3>커뮤니티</h3>
+			<ul class="list-unstyled nav-links">
+			<li><a href="https://glorylawfirm.kr/kor/revive/">회생파산센터</a></li>
+			<li><a href="https://glorylawfirm.kr/kor/divorce/">이혼/상속센터</a></li>
+			<li><a href="https://www.instagram.com/glory_lawfirm/?igshid=MzRlODBiNWFlZA%3D%3D">인스타그램</a></li>
+			<!-- <li><a href="#">페이스북</a></li>
+			<li><a href="#">유튜브</a></li> -->
+			<li><a href="https://blog.naver.com/glorylawfirm3">블로그</a></li>
+			</ul>
+			</div>
+			
+			
+			</div>
+			<div class="row ">
+			<div class="col-12 text-center">
+			<div class="copyright mt-5 pt-5">
+			<p><small>&copy; 법무법인 글로리 © All Rights Reserved.</small></p>
+			</div>
+			</div>
+			</div>
+			</div>
+			</footer>
+
+
+	
+`;
+
+document.body.appendChild(footer.content);
